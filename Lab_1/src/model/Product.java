ac5da0054ff6d71242f8bceaddc0813a26e4add7
Soham Chavan <soham_chavan@Sohams-MacBook-Air.local>
2024-09-14 01:21:08 -0400

    commited lab1

---
+++ Lab_1/src/model/Product.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author soham_chavan
+ */
+public class Product {
+    String Name;
+    String Description;
+    String Avaliblity;
+    String Price;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public String getAvaliblity() {
+        return Avaliblity;
+    }
+
+    public void setAvaliblity(String Avaliblity) {
+        this.Avaliblity = Avaliblity;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String Price) {
+        this.Price = Price;
+    }
+    
+    
+}
