7316a5ec6d1e8de317f7655b11915ef18407651f
Soham Chavan <chavan.soha@northeastern.edu>
2024-09-22 21:26:30 -0400

    Pushed lab2

---
+++ Lab_2/src/model/VitalSigns.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author sohamchavan
+ */
+public class VitalSigns {
+    String date;
+    double bloodPressure;
+    double temprature;
+    int pulse;
+
+    public double getTemprature() {
+        return temprature;
+    }
+
+    public void setTemprature(double temprature) {
+        this.temprature = temprature;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(double bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public int getPulse() {
+        return pulse;
+    }
+
+    public void setPulse(int pulse) {
+        this.pulse = pulse;
+    }
+    
+    @Override
+    public String toString(){
+    return getDate();
+    }
+    
+}
