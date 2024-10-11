a408ebb99b73768f73641b68bd45f0f967568c6a
Soham Chavan <chavan.soha@northeastern.edu>
2024-10-11 00:02:50 -0400

    Package Model Created

---
+++ Lab_4/src/model/Feature.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.Feature to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author sohamchavan
+ */
+public class Feature {
+    
+}
