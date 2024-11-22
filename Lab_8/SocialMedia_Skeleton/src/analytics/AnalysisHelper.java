13e44a33a4cb1077723b76825b84728c70b6ac26
Soham Chavan <chavan.soha@northeastern.edu>
2024-11-21 19:39:17 -0500

    MaxLikeCommentPost solved

---
+++ Lab_8/SocialMedia_Skeleton/src/analytics/AnalysisHelper.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analytics;
+
+/**
+ *
+ * @author harshalneelkamal
+ */
+
+import data.DataStore;
+import java.util.Map;
+import model.Comment;
+
+
+public class AnalysisHelper {
+    //Find Average number of likes per comment.
+    //TODO
+    public void getAverageLikesPerComments() {
+        Map<Integer, Comment> comments = DataStore.getInstance().getComments();
+        int likeNumber = 0;
+        int commentNumber = comments.size();
+        for (Comment c : comments.values()) {
+            likeNumber += c.getLikes();
+        }
+        
+        System.out.println("Average number of likes per comments: " + likeNumber / commentNumber);
+            
+    }
+    
+    public void getMaxLikeCommentPost() {
+        DataStore data = DataStore.getInstance();
+        Comment commentWithMaxLikes = null;
+        
+        for (Comment c : data.getComments().values()) {
+            if (commentWithMaxLikes == null) {
+                commentWithMaxLikes = c;
+            }
+            if (c.getLikes() > commentWithMaxLikes.getLikes()){
+                commentWithMaxLikes = c;
+            }
+        }
+        
+        int postId = commentWithMaxLikes.getPostId();
+        
+        System.out.println("Q2 - Post with most likes per comment: " + data.getPosts().get(postId).getPostId());
+    
+    }
+    
+}
